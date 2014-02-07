c9d678d9b8cf1a943fa2b023e13c767e3c78d9f0
= <=>
2014-02-07 20:54:11 +0800

    Specifications for Classes in Shell and piping

--- test/sg/edu/nus/comp/cs4218/impl/extended1/PipingToolTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/extended1/PipingToolTest.java
@@ -13,6 +13,16 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * The pipe tools allows the output of one program to be sent to the input of
+ * another program. With the help of pipe tool multiple small (and simple)
+ * programs can be connected to accomplish large number of tasks.
+ * 
+ * Command Format - PROGRAM-1-STANDARD_OUTPUT | PROGRAM-2-STANDARD_INPUT Where
+ * "|" is the pipe operator and PROGRAM-1-STANDARD_OUTPUT is the standard output
+ * of program 1 and PROGRAM-2-STANDARD_INPUT is the standard input of program 2.
+ * 
+ */
 public class PipingToolTest {
 
 	private PipingTool pipingTool;
--- test/sg/edu/nus/comp/cs4218/impl/fileutils/PWDToolTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/fileutils/PWDToolTest.java
@@ -12,7 +12,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sg.edu.nus.comp.cs4218.fileutils.IPwdTool;
-
 /**
  * Show the current directory
  * 
