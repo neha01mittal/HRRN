39ad066ac89a5e10b92e7a8589775964545330b4
Truong Phuoc <hoangphuoc25@gmail.com>
2014-02-15 22:21:19 +0800

    Implementation and test cases for wc tool

--- src/sg/edu/nus/comp/cs4218/impl/extended2/WcTool.java
+++ src/sg/edu/nus/comp/cs4218/impl/extended2/WcTool.java
@@ -1,6 +1,13 @@
 package sg.edu.nus.comp.cs4218.impl.extended2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import sg.edu.nus.comp.cs4218.extended2.IWcTool;
 import sg.edu.nus.comp.cs4218.impl.ATool;
@@ -20,38 +27,180 @@ import sg.edu.nus.comp.cs4218.impl.ATool;
 
 public class WcTool extends ATool implements IWcTool {
 
+	int readStatus = -1;
+
 	public WcTool(String[] args) {
 		super(args);
 	}
 
 	@Override
 	public String getCharacterCount(String input) {
-		// TODO Auto-generated method stub
-		return null;
+		if (input == null || readStatus == 1)
+			return "0";
+		int result = input.length();
+		setStatusCode(0);
+		return Integer.toString(result);
 	}
 
 	@Override
 	public String getWordCount(String input) {
-		// TODO Auto-generated method stub
-		return null;
+		if (input == null || readStatus == 1)
+			return "0";
+		String[] words = input.split("[ \\n]");
+		int result = words.length;
+		setStatusCode(0);
+		return Integer.toString(result);
 	}
 
 	@Override
 	public String getNewLineCount(String input) {
-		// TODO Auto-generated method stub
-		return null;
+		if (input == null || readStatus == 1)
+			return "0";
+		String[] lines = input.split("\n");
+		int result = lines.length;
+		setStatusCode(0);
+		return Integer.toString(result);
 	}
 
 	@Override
 	public String getHelp() {
-		// TODO Auto-generated method stub
-		return null;
+		String help = "-m : Print only the character counts\t" + " -w : Print only the word counts\t" + " -l : Print only the newline counts\t"
+				+ " -help : Brief information about supported options";
+		setStatusCode(0);
+		// System.out.println(help);
+		return help;
 	}
 
 	@Override
 	public String execute(File workingDir, String stdin) {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder result = new StringBuilder();
+		if ((args == null) && (stdin == null || stdin.compareTo("") == 0)) {
+			result.append("No arguments and no standard input.");
+			return result.toString();
+		}
+
+		String content = null;
+		if (stdin != null && stdin.compareTo("") != 0)
+			content = stdin;
+
+		if (args[0] != null && args[0].compareTo("-m") == 0) {
+			try {
+				characterCount(result, content);
+			} catch (IOException e) {
+				readStatus = 1;
+			}
+		} else if (args[0] != null && args[0].compareTo("-w") == 0) {
+			try {
+				wordCount(result, content);
+			} catch (IOException e) {
+				readStatus = 1;
+			}
+		} else if (args[0] != null && args[0].compareTo("-l") == 0) {
+			try {
+				lineCount(result, content);
+			} catch (IOException e) {
+				readStatus = 1;
+			}
+		} else if (args[0] != null && args[0].compareTo("-help") == 0) {
+			result.append(getHelp());
+		} else {
+			result.append("Invalid arguments.");
+			setStatusCode(1);
+		}
+		return result.toString();
 	}
 
+	private void lineCount(StringBuilder result, String content) throws IOException {
+		Charset std = Charset.forName("UTF-8");
+
+		if (content != null)
+			result.append(getNewLineCount(content));
+		else
+			for (int i = 1; i < args.length; i++) {
+				content = readFile(args[i], std);
+				if (readStatus != 1)
+					result.append(getNewLineCount(content) + " " + args[i]);
+				else
+					result.append(content);
+			}
+	}
+
+	private void wordCount(StringBuilder result, String content) throws IOException {
+		Charset std = Charset.forName("UTF-8");
+		if (content != null)
+			result.append(getWordCount(content));
+		else
+			for (int i = 1; i < args.length; i++) {
+				content = readFile(args[i], std);
+				if (readStatus != 1)
+					result.append(getWordCount(content) + " " + args[i]);
+				else
+					result.append(content);
+			}
+	}
+
+	private void characterCount(StringBuilder result, String content) throws IOException {
+		Charset std = Charset.forName("UTF-8");
+
+		if (content != null)
+			result.append(getCharacterCount(content));
+		else
+			for (int i = 1; i < args.length; i++) {
+				content = readFile(args[i], std);
+				if (readStatus != 1)
+					result.append(getCharacterCount(content) + " " + args[i]);
+				else
+					result.append(content);
+			}
+	}
+
+	/*
+	 * Read content from file
+	 */
+	public String readFile(String fileName) {
+		String filePath;
+		readStatus = -1;
+		File f = new File(fileName);
+		if (f.isAbsolute())
+			filePath = fileName;
+		else
+			filePath = System.getProperty("user.dir") + File.separator + fileName;
+
+		StringBuilder sb = new StringBuilder();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File(filePath)));
+
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			br.close();
+			readStatus = 0;
+			return sb.toString();
+		} catch (Exception e) {
+			sb.append("word count: open failed: " + fileName + ": No such file or directory.");
+			readStatus = 1;
+			setStatusCode(1);
+		}
+		return sb.toString();
+	}
+
+	public String readFile(String fileName, Charset encoding) throws IOException {
+		try {
+			String filePath;
+			readStatus = -1;
+			File f = new File(fileName);
+			if (f.isAbsolute())
+				filePath = fileName;
+			else
+				filePath = System.getProperty("user.dir") + File.separator + fileName;
+
+			byte[] encoded = Files.readAllBytes(Paths.get(filePath));
+			return encoding.decode(ByteBuffer.wrap(encoded)).toString();
+		} catch (Exception e) {
+			readStatus = 1;
+			return "word count: open failed: " + fileName + ": No such file or directory.";
+		}
+	}
 }
