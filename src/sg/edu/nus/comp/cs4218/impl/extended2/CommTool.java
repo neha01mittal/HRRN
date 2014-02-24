b4e5656e2fa5e4edb974f37d14b6d16b9d01f0d4
neha01mittal <neha01mittal@gmail.com>
2014-02-24 21:55:20 +0800

    Comm tool impl

--- src/sg/edu/nus/comp/cs4218/impl/extended2/CommTool.java
+++ src/sg/edu/nus/comp/cs4218/impl/extended2/CommTool.java
@@ -1,6 +1,11 @@
 package sg.edu.nus.comp.cs4218.impl.extended2;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import sg.edu.nus.comp.cs4218.extended2.ICommTool;
 import sg.edu.nus.comp.cs4218.impl.ATool;
@@ -21,9 +26,13 @@ import sg.edu.nus.comp.cs4218.impl.ATool;
 
 public class CommTool extends ATool implements ICommTool {
 
-	public String currentLine1;
-	public String currentLine2;
+	private static final String INVALID_COMMAND = "Invalid command";
+	private static final String NOT_SORTED = "Not Sorted!";
+	private boolean sortFlag = false;
+	String currentLine1 = "";
+	String currentLine2 = "";
 
+	// check this ^^^
 	public CommTool(String[] arguments) {
 		super(arguments);
 	}
@@ -31,31 +40,199 @@ public class CommTool extends ATool implements ICommTool {
 	@Override
 	public String compareFiles(String input1, String input2) {
 		// TODO Auto-generated method stub
-		return null;
+		return (input1.compareTo(input2) < 0 ? "-1"
+				: (input1.compareTo(input2) == 0) ? "0" : "1");
+
 	}
 
 	@Override
 	public String compareFilesCheckSortStatus(String input1, String input2) {
 		// TODO Auto-generated method stub
-		return null;
+		if (input1 == null && input2 == null)
+			return "-1";
+		if (input1 == null) {
+			if (input2.compareTo(currentLine2) > 0)
+				return "-1";
+			else
+				return NOT_SORTED;
+		} else if (input2 == null) {
+			if (input1.compareTo(currentLine1) > 0)
+				return "-1";
+			else
+				return NOT_SORTED;
+		} else if (input1.compareTo(currentLine1) > 0
+				&& input2.compareTo(currentLine2) > 0)
+			return "-1";
+		else
+			return NOT_SORTED;
 	}
 
 	@Override
 	public String compareFilesDoNotCheckSortStatus(String input1, String input2) {
 		// TODO Auto-generated method stub
-		return null;
+		return compareFiles(input1, input2);
 	}
 
 	@Override
 	public String getHelp() {
 		// TODO Auto-generated method stub
-		return null;
+		String result = "comm : Compares two sorted files line by line. With no options, produce three-column output."
+				+ "\nColumn one contains lines unique to FILE1, column two contains lines unique to FILE2,"
+				+ "\nand column three contains lines common to both files."
+				+ "\n"
+				+ "\nCommand Format - comm [OPTIONS] FILE1 FILE2"
+				+ "\nFILE1 - Name of the file 1"
+				+ "\nFILE2 - Name of the file 2"
+				+ "\n-c : check that the FILE1 is correctly sorted (increasing order) and also FILE2"
+				+ "\n if it is not correctly sorted, 'Not Sorted!' is shown"
+				+ "\n-d : do not check that the FILE1 and FILE2 are correctly sorted"
+				+ "\n-help : Brief information about supported options";
+		setStatusCode(0);
+		return result;
 	}
 
 	@Override
 	public String execute(File workingDir, String stdin) {
 		// TODO Auto-generated method stub
-		return null;
+		String file1 = "";
+		String file2 = "";
+		String operation= parse();
+		if(operation.equals(INVALID_COMMAND))
+			return INVALID_COMMAND;
+		
+		if (args != null && args.length > 0) {
+			if (operation.equalsIgnoreCase("help")) {
+				return getHelp();
+			} else if (operation.equalsIgnoreCase("d") && args.length > 2) {
+				// do not care about sorting
+				file1 = workingDir + File.separator + args[args.length - 2];
+				file2 = workingDir + File.separator + args[args.length - 1];
+			} else if (operation.equalsIgnoreCase("c") && args.length > 2) {
+				sortFlag = true;
+				file1 = workingDir + File.separator + args[args.length - 2];
+				file2 = workingDir + File.separator + args[args.length - 1];
+			} else if (args.length == 2 && operation.equals("")) {
+				sortFlag = false;
+				file1 = workingDir + File.separator + args[0];
+				file2 = workingDir + File.separator + args[1];
+			}
+
+			File f1 = new File(file1);
+			File f2 = new File(file2);
+			List<String> file1Data = readFile(f1);
+			List<String> file2Data = readFile(f2);
+
+			if(file1Data==null||file2Data==null)
+				return INVALID_COMMAND;
+			
+			String line1 = "";
+			String line2 = "";
+			String expectedOutput = "";
+
+			int i = 0;
+			int j = 0;
+			while (i < file1Data.size() || j < file2Data.size()) {
+				if (i >= file1Data.size()) {
+					line2 = file2Data.get(j);
+					if (sortFlag
+							&& !compareFilesCheckSortStatus(null, line2)
+									.equals("-1")) {
+						expectedOutput += NOT_SORTED;
+						break;
+					}
+					currentLine2 = line2;
+					expectedOutput += "\t\t" + line2;
+					j++;
+				} else if (j >= file2Data.size()) {
+					line1 = file1Data.get(i);
+					if (sortFlag
+							&& !compareFilesCheckSortStatus(line1, null)
+									.equals("-1")) {
+						expectedOutput += NOT_SORTED;
+						break;
+					}
+					currentLine1 = line1;
+					expectedOutput += line1;
+					i++;
+				} else {
+					line1 = file1Data.get(i);
+					line2 = file2Data.get(j);
+					if (sortFlag) {
+						if (!compareFilesCheckSortStatus(line1, line2).equals(
+								"-1")) {
+							expectedOutput += NOT_SORTED;
+							break;
+						}
+						currentLine1 = line1;
+						currentLine2 = line2;
+					}
+					int comparison = Integer
+							.parseInt(compareFiles(line1, line2));
+					if (comparison == 0)
+						expectedOutput += "\t\t\t\t" + line1;
+					else if (comparison < 0)
+						expectedOutput += line1 + "\n\t\t" + line2;
+					else
+						expectedOutput += "\t\t" + line2 + "\n" + line1;
+					i++;
+					j++;
+
+				}
+
+				expectedOutput += "\n";
+			}
+			if (expectedOutput.endsWith("\n"))
+				expectedOutput = expectedOutput.substring(0,
+						expectedOutput.length() - 1);
+
+			setStatusCode(0);
+			System.out.println(expectedOutput);
+			return expectedOutput;
+
+		}
+		return INVALID_COMMAND;
 	}
 
+	public String parse() {
+		String parsed = "";
+		int count=0;
+		int i = args.length - 1;
+		while (i >= 0) {
+			if (args[i].startsWith("-")) {
+				// help gets priority // if not help, the first one gets
+				// priority
+				String option = args[i].substring(1);
+				if (option.equalsIgnoreCase("help")) {
+					parsed = option;
+					break;
+				}
+				count++;
+				parsed = option;
+			}
+			
+			if(count>1) {
+				return INVALID_COMMAND;
+			}
+			i--;
+		}
+		return parsed;
+	}
+
+	private List<String> readFile(File f) {
+		List<String> expectedOutput = new ArrayList<String>();
+		if (f.isFile() && f.canRead()) {
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(f));
+				String line;
+				while ((line = br.readLine()) != null) {
+					expectedOutput.add(line);
+				}
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return expectedOutput;
+		}
+		return null;
+	}
 }
--- test/sg/edu/nus/comp/cs4218/impl/extended2/CommToolTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/extended2/CommToolTest.java
@@ -1,7 +1,6 @@
 package sg.edu.nus.comp.cs4218.impl.extended2;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,245 +9,285 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import sg.edu.nus.comp.cs4218.impl.extended2.CommTool;
 
 public class CommToolTest {
 
-	private CommTool commTool;
 
+	private CommTool commTool;
+	
 	@Before
-	public void before() {
+	public void before(){
 		commTool = new CommTool(null);
 	}
 
-	@After
-	public void after() {
-		commTool = null;
+    @After
+	public void after(){
+    	commTool = null;
 	}
-
-	// test interface methods
-	@Test
-	public void executeHelpTest() {
-		String[] args = { "-c", "-help", "commTestCase1a.txt" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-		String result = "comm : Compares two sorted files line by line. With no options, produce three-column output."
-				+ "\nColumn one contains lines unique to FILE1, column two contains lines unique to FILE2,"
-				+ "\nand column three contains lines common to both files." + "\n" + "\nCommand Format - comm [OPTIONS] FILE1 FILE2"
-				+ "\nFILE1 - Name of the file 1" + "\nFILE2 - Name of the file 2"
+    
+    
+    // test interface methods
+    @Test
+   	public void executeHelpTest() {
+       	String [] args = {"-c", "-help", "commTestCase1a.txt"};
+   		commTool = new CommTool(args);
+   		String workingDir = System.getProperty("user.dir");
+   		String result = "comm : Compares two sorted files line by line. With no options, produce three-column output."
+				+ "\nColumn one contains lines unique to FILE1, column two contains lines unique to FILE2," 
+				+ "\nand column three contains lines common to both files."
+				+ "\n"
+				+ "\nCommand Format - comm [OPTIONS] FILE1 FILE2"
+				+ "\nFILE1 - Name of the file 1"
+				+ "\nFILE2 - Name of the file 2"
 				+ "\n-c : check that the FILE1 is correctly sorted (increasing order) and also FILE2"
-				+ "\n if it is not correctly sorted, 'Not Sorted!' is shown" + "\n-d : do not check that the FILE1 and FILE2 are correctly sorted"
+				+ "\n if it is not correctly sorted, 'Not Sorted!' is shown"
+				+ "\n-d : do not check that the FILE1 and FILE2 are correctly sorted"
 				+ "\n-help : Brief information about supported options";
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), result);
-		assertEquals(commTool.getStatusCode(), 0);
+   		File f = new File(workingDir);
+   		assertEquals(commTool.execute(f, null), result);
+   		assertEquals(commTool.getStatusCode(), 0);
 
-	}
-
-	@Test
+      }
+    
+    @Test
 	public void executeNoOptionTest() {
-		String[] args = { "commTestCase1a.txt", "commTestCase1b.txt" };
-		commTool = new CommTool(args);
+    	String [] args = {"commTestCase1a.txt", "commTestCase1b.txt"};
+    	commTool = new CommTool(args);
 		String workingDir = System.getProperty("user.dir");
-		String output = "\t\t\t\taaa" + "\n\t\tbbb" + "\nccc" + "\neee" + "\n\t\tffff" + "\ngggggg";
+		String output = "\t\t\t\taaa"
+						+"\n\t\tbbb"
+						+"\nccc"
+						+"\neee"
+						+"\n\t\tffff"
+						+"\ngggggg";
 		File f = new File(workingDir);
 		assertEquals(commTool.execute(f, null), output);
 		assertEquals(commTool.getStatusCode(), 0);
 
-	}
+    }
+    
+    @Test
+   	public void executeNoOptionTest1() {
+       	String [] args = {"commTestCase1b.txt", "commTestCase1a.txt"};
+       	commTool = new CommTool(args);
+   		String workingDir = System.getProperty("user.dir");
+   		String output = "\t\t\t\taaa"
+   						+"\nbbb"
+   						+"\n\t\tccc"
+   						+"\n\t\teee"
+   						+"\nffff"
+   						+"\n\t\tgggggg";
+   		File f = new File(workingDir);
+   		assertEquals(commTool.execute(f, null), output);
+   		assertEquals(commTool.getStatusCode(), 0);
 
-	@Test
-	public void executeNoOptionTest1() {
-		String[] args = { "commTestCase1b.txt", "commTestCase1a.txt" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-		String output = "\t\t\t\taaa" + "\nbbb" + "\n\t\tccc" + "\n\t\teee" + "\nffff" + "\n\t\tgggggg";
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), output);
-		assertEquals(commTool.getStatusCode(), 0);
-
-	}
-
-	@Test
+       }
+    
+    @Test
 	public void executeDoNotCheckSortTest() {
-		String[] args = { "-d", "commTestCase1a.txt", "commTestCase1b.txt" };
-		commTool = new CommTool(args);
+    	String [] args = {"-d", "commTestCase1a.txt", "commTestCase1b.txt"};
+    	commTool = new CommTool(args);
 		String workingDir = System.getProperty("user.dir");
-		String output = "\t\t\t\taaa" + "\n\t\tbbb" + "\nccc" + "\neee" + "\n\t\tffff" + "\ngggggg";
+		String output = "\t\t\t\taaa"
+						+"\n\t\tbbb"
+						+"\nccc"
+						+"\neee"
+						+"\n\t\tffff"
+						+"\ngggggg";
 		File f = new File(workingDir);
 		assertEquals(commTool.execute(f, null), output);
 		assertEquals(commTool.getStatusCode(), 0);
 
-	}
+    }
+    
+    @Test
+   	public void executeDoNotCheckSortTest1() {
+       	String [] args = {"-d", "commTestCase1b.txt", "commTestCase1a.txt"};
+       	commTool = new CommTool(args);
+   		String workingDir = System.getProperty("user.dir");
+   		String output = "\t\t\t\taaa"
+   						+"\nbbb"
+   						+"\n\t\tccc"
+   						+"\n\t\teee"
+   						+"\nffff"
+   						+"\n\t\tgggggg";
+   		File f = new File(workingDir);
+   		assertEquals(commTool.execute(f, null), output);
+   		assertEquals(commTool.getStatusCode(), 0);
 
-	@Test
-	public void executeDoNotCheckSortTest1() {
-		String[] args = { "-d", "commTestCase1b.txt", "commTestCase1a.txt" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-		String output = "\t\t\t\taaa" + "\nbbb" + "\n\t\tccc" + "\n\t\teee" + "\nffff" + "\n\t\tgggggg";
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), output);
-		assertEquals(commTool.getStatusCode(), 0);
-
-	}
-
-	@Test
+       }
+    
+    @Test
 	public void executeCheckSortTest() {
-		String[] args = { "-c", "commTestCase1a.txt", "commTestCase1b.txt" };
-		commTool = new CommTool(args);
+    	String [] args = {"-c", "commTestCase1a.txt", "commTestCase1b.txt"};
+    	commTool = new CommTool(args);
 		String workingDir = System.getProperty("user.dir");
-		String output = "\t\t\t\taaa" + "\n\t\tbbb" + "\nccc" + "\neee" + "\n\t\tffff" + "\ngggggg";
+		String output = "\t\t\t\taaa"
+						+"\n\t\tbbb"
+						+"\nccc"
+						+"\neee"
+						+"\n\t\tffff"
+						+"\ngggggg";
 		File f = new File(workingDir);
 		assertEquals(commTool.execute(f, null), output);
 		assertEquals(commTool.getStatusCode(), 0);
 
-	}
+    }
+    @Test
+   	public void executeCheckSortTest1() {
+       	String [] args = {"-c", "commTestCase1b.txt", "commTestCase1a.txt"};
+       	commTool = new CommTool(args);
+   		String workingDir = System.getProperty("user.dir");
+   		String output = "\t\t\t\taaa"
+   						+"\nbbb"
+   						+"\n\t\tccc"
+   						+"\n\t\teee"
+   						+"\nffff"
+   						+"\n\t\tgggggg";
+   		File f = new File(workingDir);
+   		assertEquals(commTool.execute(f, null), output);
+   		assertEquals(commTool.getStatusCode(), 0);
 
-	@Test
-	public void executeCheckSortTest1() {
-		String[] args = { "-c", "commTestCase1b.txt", "commTestCase1a.txt" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-		String output = "\t\t\t\taaa" + "\nbbb" + "\n\t\tccc" + "\n\t\teee" + "\nffff" + "\n\t\tgggggg";
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), output);
-		assertEquals(commTool.getStatusCode(), 0);
+     }
+    @Test
+   	public void executeCheckSortTest2() {
+       	String [] args = {"-c", "commTestCase2a.txt", "commTestCase2b.txt"};
+       	commTool = new CommTool(args);
+   		String workingDir = System.getProperty("user.dir");
+   		String output = "bbb"
+   						+"\n\t\tccc"
+   						+"\n\t\t\t\tddd"
+   						+"\n\t\teee"
+   						+"\nfff"
+   						+"\nNot Sorted!";
+   		File f = new File(workingDir);
+   		assertEquals(commTool.execute(f, null), output);
+   		assertEquals(commTool.getStatusCode(), 0);
 
-	}
+     }
+    @Test
+   	public void executeCheckSortTest3() {
+       	String [] args = {"-c", "commTestCase2b.txt", "commTestCase2a.txt"};
+       	commTool = new CommTool(args);
+   		String workingDir = System.getProperty("user.dir");
+   		String output = "\t\tbbb"
+   						+"\nccc"
+   						+"\n\t\t\t\tddd"
+   						+"\neee"
+   						+"\n\t\tfff"
+   						+"\nNot Sorted!";
+   		File f = new File(workingDir);
+   		assertEquals(commTool.execute(f, null), output);
+   		assertEquals(commTool.getStatusCode(), 0);
 
-	@Test
-	public void executeCheckSortTest2() {
-		String[] args = { "-c", "commTestCase2a.txt", "commTestCase2b.txt" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-		String output = "bbb" + "\n\t\tccc" + "\n\t\t\t\tddd" + "\n\t\teee" + "\nfff" + "\nNot Sorted!";
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), output);
-		assertEquals(commTool.getStatusCode(), 0);
-
-	}
-
-	@Test
-	public void executeCheckSortTest3() {
-		String[] args = { "-c", "commTestCase2b.txt", "commTestCase2a.txt" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-		String output = "\t\tbbb" + "\nccc" + "\n\t\t\t\tddd" + "\neee" + "\n\t\tfff" + "\nNot Sorted!";
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), output);
-		assertEquals(commTool.getStatusCode(), 0);
-
-	}
-
-	@Test
+     }
+    
+    @Test
 	public void executeMutipleOptionsTest() throws IOException {
-		String[] args = { "-c", "-d", "uniqTestCase1b.txt", "uniqTestCase1a.txt" };
+    	//@CORRECTED - changed file name to uniqTestCase1 and 2 from uniqTestCase1b and 1a (No files with those names were given to us)
+		String [] args = {"-c", "-d","uniqTestCase1.txt","uniqTestCase2.txt"};
 		commTool = new CommTool(args);
 		String workingDir = System.getProperty("user.dir");
-
+		
 		File f = new File(workingDir);
 		assertEquals(commTool.execute(f, null), "Invalid command");
 		assertEquals(commTool.getStatusCode(), 1);
-
-	}
-
-	@Test
-	public void executeInvalidOptionTest() throws IOException {
-		String[] args = { "-" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), "Invalid command");
-		assertEquals(commTool.getStatusCode(), 1);
-	}
-
-	@Test
+		
+    }
+    
+    @Test
+  	public void executeInvalidOptionTest() throws IOException {
+  		String [] args = {"-"};
+  		commTool = new CommTool(args);
+  		String workingDir = System.getProperty("user.dir");
+  		
+  		File f = new File(workingDir);
+  		assertEquals(commTool.execute(f, null), "Invalid command");
+  		assertEquals(commTool.getStatusCode(), 1);
+     }
+    
+    @Test
 	public void executeInvalidFileTest() throws IOException {
-		String[] args = { "-i", "commTestCase1a.txt", "input2q3t.tx3t" };
+		String [] args = {"-i","commTestCase1a.txt", "input2q3t.tx3t"};
 		commTool = new CommTool(args);
 		String workingDir = System.getProperty("user.dir");
-
+		
 		File f = new File(workingDir);
 		assertEquals(commTool.execute(f, null), "Invalid command");
 		assertEquals(commTool.getStatusCode(), 1);
-	}
-
-	@Test
-	public void executeSameFileTest() throws IOException {
-		String[] args = { "-c", "commTestCase1a.txt", "commTestCase1a.txt" };
-		commTool = new CommTool(args);
-		String workingDir = System.getProperty("user.dir");
-		String output = "\t\t\t\taaa" + "\n\t\t\t\tccc" + "\n\t\t\t\teee" + "\n\t\t\t\tgggggg";
-		File f = new File(workingDir);
-		assertEquals(commTool.execute(f, null), output);
-		assertEquals(commTool.getStatusCode(), 0);
-	}
-
-	@Test
-	public void compareFilesDifferentInputTest() {
-		String input1 = "abc aed";
-		String input2 = "acdef fe";
-
-		assertTrue(Integer.parseInt(commTool.compareFiles(input1, input2)) < 0);
-	}
-
-	@Test
-	public void compareFilesSameInputTest() {
-		String input1 = "1245 ggg";
-		String input2 = "1245 ggg";
-
-		assertTrue(Integer.parseInt(commTool.compareFiles(input1, input2)) == 0);
-	}
-
-	@Test
-	public void compareFilesDoNotCheckSortStatusDifferentInputTest() {
-		String input1 = "acdef 124";
-		String input2 = "abc 125";
-
-		assertTrue(Integer.parseInt(commTool.compareFilesDoNotCheckSortStatus(input1, input2)) > 0);
-	}
-
-	@Test
-	public void compareFilesDoNotCheckSortStatusSameInputTest() {
-		String input1 = "1245 ggg";
-		String input2 = "1245 ggg";
-
-		assertTrue(Integer.parseInt(commTool.compareFilesDoNotCheckSortStatus(input1, input2)) == 0);
-	}
-
-	@Test
-	public void compareFilesCheckSortStatusSortedTest() {
-		commTool.currentLine1 = "abc";
-		String input1 = "abc ";
-
-		commTool.currentLine2 = "ab efh";
-		String input2 = "acdef";
-
-		assertTrue(Integer.parseInt(commTool.compareFilesCheckSortStatus(input1, input2)) < 0);
-	}
-
-	@Test
-	public void compareFilesCheckSortStatusInput1UnsortedTest() {
-		commTool.currentLine1 = "abc ewr";
-		String input1 = "abc ";
-
-		commTool.currentLine2 = "ab efh";
-		String input2 = "acdef";
-
-		assertEquals(commTool.compareFilesCheckSortStatus(input1, input2), "Not Sorted!");
-	}
-
-	@Test
-	public void compareFilesCheckSortStatusInput2UnsortedTest() {
-		commTool.currentLine1 = "abc";
-		String input1 = "abc ";
-
-		commTool.currentLine2 = "eifjefe epfamfe";
-		String input2 = "acdef";
-
-		assertEquals(commTool.compareFilesCheckSortStatus(input1, input2), "Not Sorted!");
-	}
+    }
+    @Test
+  	public void executeSameFileTest() throws IOException {
+  		String [] args = {"-c","commTestCase1a.txt", "commTestCase1a.txt"};
+  		commTool = new CommTool(args);
+  		String workingDir = System.getProperty("user.dir");
+  		String output = "\t\t\t\taaa"
+					+"\n\t\t\t\tccc"
+					+"\n\t\t\t\teee"
+					+"\n\t\t\t\tgggggg";
+  		File f = new File(workingDir);
+  		assertEquals(commTool.execute(f, null), output);
+  		assertEquals(commTool.getStatusCode(), 0);
+     }
+    
+    @Test
+   	public void compareFilesDifferentInputTest(){
+   		String input1 = "abc aed";
+   		String input2 = "acdef fe";
+   		
+   		assertTrue(Integer.parseInt(commTool.compareFiles(input1, input2)) < 0);
+    }
+    @Test
+   	public void compareFilesSameInputTest(){
+   		String input1 = "1245 ggg";
+   		String input2 = "1245 ggg";
+   		
+   		assertTrue(Integer.parseInt(commTool.compareFiles(input1, input2)) == 0);
+    }
+    @Test
+   	public void compareFilesDoNotCheckSortStatusDifferentInputTest(){
+   		String input1 = "acdef 124";
+   		String input2 = "abc 125";
+   		
+   		assertTrue(Integer.parseInt(commTool.compareFilesDoNotCheckSortStatus(input1, input2)) > 0);
+    }
+    @Test
+   	public void compareFilesDoNotCheckSortStatusSameInputTest(){
+   		String input1 = "1245 ggg";
+   		String input2 = "1245 ggg";
+   		
+   		assertTrue(Integer.parseInt(commTool.compareFilesDoNotCheckSortStatus(input1, input2)) == 0);
+    }
+    @Test
+	public void compareFilesCheckSortStatusSortedTest(){
+    	commTool.currentLine1 = "abc";
+   		String input1 = "abc ";
+   		
+   		commTool.currentLine2 = "ab efh";
+   		String input2 = "acdef";
+   		
+   		assertTrue(Integer.parseInt(commTool.compareFilesCheckSortStatus(input1, input2)) < 0);
+    }
+    @Test
+	public void compareFilesCheckSortStatusInput1UnsortedTest(){
+    	commTool.currentLine1 = "abc ewr";
+   		String input1 = "abc ";
+   		
+   		commTool.currentLine2 = "ab efh";
+   		String input2 = "acdef";
+   		
+   		assertEquals(commTool.compareFilesCheckSortStatus(input1, input2), "Not Sorted!");
+    }
+    @Test
+	public void compareFilesCheckSortStatusInput2UnsortedTest(){
+    	commTool.currentLine1 = "abc";
+   		String input1 = "abc ";
+   		
+   		commTool.currentLine2 = "eifjefe epfamfe";
+   		String input2 = "acdef";
+   		
+   		assertEquals(commTool.compareFilesCheckSortStatus(input1, input2), "Not Sorted!");
+    }
+    
 
 }
