c9d8fdc52fb37c4a686e7e3790e364be54083659
neha01mittal <neha01mittal@gmail.com>
2014-03-07 13:11:17 +0800

    all test cases

---
+++ test/sg/edu/nus/comp/cs4218/impl/integrate/CatToolIntegrationTest.java
@@ -0,0 +1,325 @@
+package sg.edu.nus.comp.cs4218.impl.integrate;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import sg.edu.nus.comp.cs4218.impl.extended1.PipingTool;
+import sg.edu.nus.comp.cs4218.impl.utils.TestUtils;
+
+public class CatToolIntegrationTest {
+
+	// ls, pwd, cd, copy, delete, move, paste, comm, grep, wc, uniq, sort, copy
+
+	private static File rootParent;
+	private static File f1;
+	private static File f2;
+	private static File f3;
+	private static File file1;
+	private static File file2;
+
+	@BeforeClass
+	public static void init() {
+		rootParent = new File(System.getProperty("user.dir"));
+		f1 = new File(rootParent, "testFolder");
+		f2 = new File(f1, "testFolder2");
+		file1 = new File(f1, "test1.txt");
+		file2 = new File(f1, "test2.txt");
+		
+		f3 = new File(f2,"testFolder3");
+		f1.mkdir();
+		f2.mkdir();
+		f3.mkdir();
+		try {
+			file1.createNewFile();
+			file2.createNewFile();
+			create(file2.getAbsolutePath(),"This is file 2 content");
+			create(file1.getAbsolutePath(),"This is test content and more");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public static void afterClass() throws IOException {
+		TestUtils.delete(f1);
+	}
+
+	@Test
+	public void testLs() {
+		String commandline = "ls | cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "test1.txt\ntest2.txt\ntestFolder2";
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void testPwd() {
+		String commandline = "pwd | cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = f1.getAbsolutePath();
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testPwd2() {
+		String commandline = "pwd rubbishvalue | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output= pipingTool.execute(f1, null);
+		String expectedOutput = f1.getAbsolutePath();
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCd() {
+		String commandline = "cd .. | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output=pipingTool.execute(f2, null);
+		System.out.println(output);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCd2() {
+		String commandline = "cd "+f2+"| cat test1.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output=pipingTool.execute(f1, null);
+		String expectedOutput = "This is test content and more\n";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testMove() {
+		String commandline = "move " + file1 + " " + f1 + "| cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output=pipingTool.execute(f1, null);
+		System.out.println(output);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+		file1.mkdir();
+	}
+
+	@Test
+	public void testMove2() {
+		String commandline = "move " + file1 +  "| cat test1.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is test content and more\n";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCopy() {
+		String commandline = "copy  | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCopy2() {
+		String commandline = "copy test1.txt testFolder2 | cat test2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is file 2 content\n";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testDelete() throws IOException {
+		String commandline = "delete " + file1.getName() + "| cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f3, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+		file1.createNewFile();
+		create(file1.getAbsolutePath(),"This is test content and more\n");
+	}
+	@Test
+	public void testDelete2() {
+		String commandline = "delete testFolder2 | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+		f2.mkdirs();
+	}
+
+	@Test
+	public void testEcho() {
+		String commandline = "echo This is test | cat test2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is file 2 content\n";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testEcho2() {
+		String commandline = "echo | cat - repeat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCat() {
+		String commandline = "cat " + file2 + "| cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is file 2 content\n";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCat2() {
+		String commandline = "cat "+ file1 +" "+ file2+" | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is test content and more\nThis is file 2 content\n";
+		assertEquals(expectedOutput, output);
+	}
+	
+	@Test
+	public void testWc() {
+		// breaking
+		String commandline = "wc -l test1.txt | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		System.out.println(output);
+		String expectedOutput = "0 test1.txt";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testWc2() {
+		String commandline = "wc -x test1.txt | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testUniq() {
+		String commandline = "uniq -f -1 test1.txt | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		System.out.println(output);
+		String expectedOutput = "This is test content and more";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testUniq2() {
+		String commandline = "uniq -f -1 test2.txt | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is file 2 content";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testPaste() {
+		String commandline = "paste " + file1 + " " + file2 + "| cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is test content and more	This is file 2 content";
+		assertEquals(expectedOutput, output);
+	} 
+
+	@Test
+	public void testPaste2() {
+		String commandline = "paste " + file1 + "| cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "This is test content and more";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCut() {
+		String commandline = "cut -f "+file1+ "| cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCut2() {
+		String commandline = "cut -f -c "+file1+ "| cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testSort() {
+		String commandline = "sort -c " + file1 + "| cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "In order.";
+		assertEquals(expectedOutput, output);
+	}
+	@Test
+	public void testSort2() {
+		String commandline = "sort | cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testComm() {
+		String commandline = "comm test1.txt test2.txt | cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		System.out.println(output);
+		String expectedOutput = "\t\tThis is file 2 content\nThis is test content and more";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testComm2() {
+		String commandline = "comm -d test1.txt test2.txt | cat";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(f1, null);
+		System.out.println(output);
+		String expectedOutput = "\t\tThis is file 2 content\nThis is test content and more";
+		assertEquals(expectedOutput, output);
+	}
+	
+	public static void create(String filename, String content) {
+		Writer writer = null;
+
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "utf-8"));
+			writer.write(content);
+		} catch (IOException ex) {
+			// report
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception ex) {
+			}
+		}
+	}
+}
--- test/sg/edu/nus/comp/cs4218/impl/integrate/CommToolIntegrationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/integrate/CommToolIntegrationTest.java
@@ -2,7 +2,12 @@ package sg.edu.nus.comp.cs4218.impl.integrate;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -10,23 +15,25 @@ import org.junit.Test;
 import sg.edu.nus.comp.cs4218.impl.extended1.PipingTool;
 
 public class CommToolIntegrationTest {
- // get two cases from pipe (aalr impl)
-	
+	// get two cases from pipe (aalr impl)
+
 	private PipingTool pipingTool;
 	private static String rootParent;
-	
+
 	@BeforeClass
 	public static void init() {
-		rootParent = System.getProperty("user.dir");
-		
+		rootParent = System.getProperty("user.dir") + File.separator
+				+ "testFiles";
+
 	}
+
 	@Test
-	public void testPipeCommTool() {
+	public void testCatTool() {
 		String commandline = "cat commTestCase1a.txt | comm - commTestCase1b.txt ";
 		String output = "\t\t\t\taaa" + "\n\t\tbbb" + "\nccc" + "\neee"
 				+ "\n\t\tffff" + "\ngggggg";
-	    pipingTool = new PipingTool(commandline.split("\\|"));
-	    File workingDir = new File(rootParent);
+		pipingTool = new PipingTool(commandline.split("\\|"));
+		File workingDir = new File(rootParent);
 
 		String result = pipingTool.execute(workingDir, null);
 
@@ -34,7 +41,7 @@ public class CommToolIntegrationTest {
 	}
 
 	@Test
-	public void testPipeCommTool1() {
+	public void testPipeCatTool1() {
 		String commandline = "cat commTestCase1a.txt | comm - nofile.txt ";
 		String output = "\t\t\t\taaa" + "\n\t\tbbb" + "\nccc" + "\neee"
 				+ "\n\t\tffff" + "\ngggggg";
@@ -43,5 +50,226 @@ public class CommToolIntegrationTest {
 		String result = pipingTool.execute(new File(rootParent), null);
 
 		assertEquals(result, "");
+	}
+
+	@Test
+	public void testLs() {
+		String commandline = "ls | comm - commTestCase1b.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		System.out.println(output);
+		String expectedOutput = "\t\taaa\ncommTestCase1a.txt\n\t\tbbb\ncommTestCase1b.txt\n\t\tffff\nlvl1folder\ntestCase_2.txt\ntestCase_3.txt\ntestCase_4.txt\ntestCase_5.txt";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testPwd() {
+		String commandline = "pwd | comm - commTestCase1a.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = rootParent+"\n\t\taaa\n\t\tccc\n\t\teee\n\t\tgggggg";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testPwd2() {
+		String commandline = "pwd | comm -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCd() {
+		String commandline = "cd .. | comm - commTestCase1b.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCd2() {
+		String commandline = "cd | comm - nofile.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testMove() {
+		String commandline = "move testCase_2.txt lvl1folder | comm - testCase_2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+		
+	}
+
+	@Test
+	public void testMove2() {
+		String commandline = "move testCase_3.txt | comm -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCopy() {
+		String commandline = "copy testCase_2.txt lvl1folder | comm -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testCopy2() {
+		String commandline = "copy testCase_2.txt  | comm -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testDelete() throws IOException {
+		String commandline = "delete testCase_5.txt | comm - testCase_2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+		File f = new File(rootParent,"testCase_5.txt");
+		f.createNewFile();
+		create(f.getAbsolutePath(), "apple\nban\nbanana\ncarrot\ncarro\nc");
+	}
+
+	@Test
+	public void testDelete2() {
+		String commandline = "delete | comm";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testEcho() {
+		String commandline = "echo aab | comm - commTestCase1b.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "\t\taaa"+ "\naab" + "\n\t\tbbb" + "\n\t\tffff";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testEcho2() {
+		String commandline = "echo | comm -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testWc() {
+		// breaking
+		String commandline = "wc -l testCase_3.txt | comm - testCase_2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		System.out.println(output);
+		String expectedOutput = "5 testCase_3.txt"+
+		"\n\t\tgek1517" +
+		"\n\t\tACC1002X" +
+		"\n\t\tsw2104" +
+		"\n\t\tpc1141";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testWc2() {
+		String commandline = "wc -x test1.txt | comm - testCase_2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testUniq() {
+		String commandline = "uniq -f -1 testCase_3.txt | comm - testCase_2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		System.out.println(output);
+		String expectedOutput = "a\n\t\tgek1517\n\t\tACC1002X\nb\nc\n\t\tsw2104\n\t\tpc1141";
+		assertEquals(expectedOutput, output);
+	}
+
+
+	@Test
+	public void testUniq2() {
+		String commandline = "uniq -f -1 test2.txt | comm ";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testPaste2() {
+		String commandline = "paste commTestCase1a.txt | comm - commTestCase1b.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "\t\t\t\taaa" + "\n\t\tbbb" + "\nccc" + "\neee"
+				+ "\n\t\tffff" + "\ngggggg";
+		assertEquals(expectedOutput, output);
+	}
+
+
+	@Test
+	public void testCut2() {
+		// check this one 
+		String commandline = "cut -f -c testCase_2.txt | comm - testCase_3.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testSort() {
+		String commandline = "sort -c testCase_3.txt | comm - testCase_2.txt";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "In order.\n\t\tgek1517\n\t\tACC1002X\n\t\tsw2104\n\t\tpc1141";
+		assertEquals(expectedOutput, output);
+	}
+
+	@Test
+	public void testSort2() {
+		String commandline = "sort | cat -";
+		PipingTool pipingTool = new PipingTool(commandline.split("\\|"));
+		String output = pipingTool.execute(new File(rootParent), null);
+		String expectedOutput = "";
+		assertEquals(expectedOutput, output);
+	}
+	
+	public static void create(String filename, String content) {
+		Writer writer = null;
+
+		try {
+			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "utf-8"));
+			writer.write(content);
+		} catch (IOException ex) {
+			// report
+		} finally {
+			try {
+				writer.close();
+			} catch (Exception ex) {
+			}
+		}
 	}
 }
